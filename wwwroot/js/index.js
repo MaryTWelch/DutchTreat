82cb0dcecf89408e1e5c7a00fbacc2acb15b8947
Mary Welch <Mary.Welch@artisantechgroup.com>
2021-01-21 06:11:32 -0600

    more jquery

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -7,9 +7,11 @@ var theForm = $("#theForm");
 theForm.hide();
 
 var button = document.getElementById('buyButton');
-button.addEventListener("click", function () {
+button.on("click", function () {
     console.log("Buying Item");
 });
 
-var productInfo = document.getElementsByClassName("product-props");
-var listItems = productInfo.item[0].children;
+var productInfo = $(".product-props li");
+productInfo.on("click", function () {
+    console.log("You clicked on " + $(this).text());
+});
