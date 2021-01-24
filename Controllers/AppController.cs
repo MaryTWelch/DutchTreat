eecc9b333307048f521258cf04f3b41a01172d6a
Mary Welch <Mary.Welch@artisantechgroup.com>
2021-01-23 18:55:21 -0600

    MVC, Controller, Razor page, routing setup

---
+++ Controllers/AppController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DutchTreat.Controllers
+{
+  public class AppController: Controller
+  {
+    public IActionResult Index()
+    {
+      return View();
+    }
+  }
+}
