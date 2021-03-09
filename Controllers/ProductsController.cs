d28a80ca582f2840116766cd5233bae9718e6b1f
Mary Welch <Mary.Welch@artisantechgroup.com>
2021-03-08 20:37:35 -0600

    working on building Products Controller

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -3,11 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DutchTreat.Data;
+using DutchTreat.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace DutchTreat.Controllers
 {
-    public class ProductsController
+    [Route("api/[Controller]")]
+    public class ProductsController: Controller
     {
-        
+        private readonly IDutchRepository _repository;
+        private readonly ILogger<ProductsController> _logger;
+        public ProductsController(IDutchRepository repository, ILogger<ProductsController> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return _repository.GetAllProducts();
+        }
     }
 }
